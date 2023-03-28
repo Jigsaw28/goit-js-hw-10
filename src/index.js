b611fe6d6c763ff9e9f85f7acf9143cb4076970f
Jigsaw28 <levenko7@gmail.com>
2023-03-28 23:14:23 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 
-const inputEl = document.querySelector("#search-box");
-console.log(inputEl)
+const refs = {
+    inputEl: document.querySelector("#search-box"),
+    countryList: document.querySelector(".country-list"),
+    countryInfo: document.querySelector(".country-info")
+}
 
+refs.inputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput() {
+    fetchCountries(name)
+        .then((name) => renderNameList(name))
+        .catch((error) => console.log(error));
+}
+
+function renderNameList(names) {
+    const markup = names.map((name) => {
+        return `<p><b>Name</b>: ${name.name}</p>
+        <p><b>Name</b>: ${name.capital}</p>
+        <p><b>Name</b>: ${name.population}</p>
+        <p><b>Name</b>: ${name.flags}</p>
+        <p><b>Name</b>: ${name.languages}</p>`
+    })
+        .join("");
+    refs.countryInfo.innerHTML = markup;
+}
